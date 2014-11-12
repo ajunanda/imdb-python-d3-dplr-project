25ec09d0fd8cf39e8fd22a32bacd04ae97065583
Robert Chang <robert8138@gmail.com>
2014-11-11 22:02:29 -0800

    Adding capability to loop through pages to scrape data

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -2,33 +2,54 @@ import requests
 from pattern import web
 from BeautifulSoup import BeautifulSoup
 
+NUMBER_MOVIES_BY_VOTES = 228971
+NUMBER_MOVIES_PER_PAGE = 50
+NUMBER_PAGES_BY_VOTES = NUMBER_MOVIES_BY_VOTES / NUMBER_MOVIES_PER_PAGE
+
+
 # Explicit URL
 url = "http://www.imdb.com/search/title?at=0&sort=num_votes&title_type=feature&year=1950,2014"
 r = requests.get(url)
 print r.url
 
 # Base URL with GET requests wrapped in a dictionary
-url = 'http://www.imdb.com/search/title'
-params = dict(sort = 'num_votes', at = 0, title_type = 'feature', year = '1950,2014')
-r = requests.get(url, params = params)
-print r.url
+#base_url = 'http://www.imdb.com/search/title'
+# params = dict(sort = 'num_votes', at = 0, title_type = 'feature', year = '1950,2014')
+# r = requests.get(base_url, params = params)
+# print r.url
 
-dom = web.Element(r.text)
-for movie in dom.by_tag('td.title')[0:10]:
-	title = movie.by_tag('a')[0].content
-	year = movie.by_tag('span.year_type')[0].content.strip('(').strip(')')
-	genres = movie.by_tag('span.genre')[0].by_tag('a')
-	genres = [g.content for g in genres]
-	runtime = movie.by_tag('span.runtime')[0].content
-	# get rating
-	rating_list = movie.by_tag('div.rating-list')[0]
-	rating_str = rating_list.title
-	rating_end_idx = a.rindex('/')
-	rating = rating_str[rating_end_idx - 3: rating_end_idx]
-	# get votes
-	votes_start_index = rating_str.rindex('(')
-	votes_end_index = rating_str.rindex(')')
-	votes = rating_str[votes_start_index + 1 : votes_end_index]
-	votes = int(votes.strip('votes ').replace(',',''))
-	# print the results
-	print title, year, genres, runtime, rating, votes
+def extract_dom(at, url = 'http://www.imdb.com/search/title'):
+	params = dict(sort = 'num_votes', at = 0, title_type = 'feature', year = '1950,2014')
+	r = requests.get(url, params = params)
+	print r.url
+	return web.Element(r.text)
+
+def extract_rating(div):
+	rating_str = div.title
+	end_idx = rating_str.rindex('/')
+	return rating_str[end_idx - 3 : end_idx]
+
+def extract_num_votes(div):
+	num_votes_str = div.title
+	start_idx = num_votes_str.rindex('(')
+	end_idx = num_votes_str.rindex(')')
+	num_votes = num_votes_str[start_idx + 1 : end_idx]
+	return int(num_votes.strip('votes ').replace(',', ''))
+
+for at in range(NUMBER_PAGES_BY_VOTES + 1):
+	print "Currenting processing page number: " + str(at) + "..."
+	dom = extract_dom(at, base_url)
+
+	for movie in dom.by_tag('td.title'):
+		title = movie.by_tag('a')[0].content
+		year = movie.by_tag('span.year_type')[0].content.strip('(').strip(')')
+		genres = movie.by_tag('span.genre')[0].by_tag('a')
+		genres = [g.content for g in genres]
+		runtime = movie.by_tag('span.runtime')[0].content
+		rating_list = movie.by_tag('div.rating-list')[0]
+		# get rating
+		rating = extract_rating(rating_list)
+		# get votes
+		num_votes = extract_num_votes(rating_list)
+		# print the results
+		print title, year, genres, runtime, rating, votes
