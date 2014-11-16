771765586de650983416ff8d0f87d33d367e817c
Robert Chang <robert8138@gmail.com>
2014-11-15 17:10:24 -0800

    Extend scraper to get box_office_number, crew members name, certificate. TODO: handle missing value edge cases

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -1,28 +1,43 @@
+import csv
+import re
 import requests
 from pattern import web
-from BeautifulSoup import BeautifulSoup
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+# CONSTANTS
+#######################################################################
 NUMBER_MOVIES_BY_VOTES = 228971
 NUMBER_MOVIES_PER_PAGE = 50
 NUMBER_PAGES_BY_VOTES = NUMBER_MOVIES_BY_VOTES / NUMBER_MOVIES_PER_PAGE
-
+ONE_MILLION = 1000000
 
 # Explicit URL
+#######################################################################
 url = "http://www.imdb.com/search/title?at=0&sort=num_votes&title_type=feature&year=1950,2014"
-r = requests.get(url)
-print r.url
+url_box_office = "http://www.imdb.com/search/title?at=0&sort=boxoffice_gross_us&title_type=feature&year=1950,2014"
+base_url = 'http://www.imdb.com/search/title'
 
-# Base URL with GET requests wrapped in a dictionary
-#base_url = 'http://www.imdb.com/search/title'
-# params = dict(sort = 'num_votes', at = 0, title_type = 'feature', year = '1950,2014')
-# r = requests.get(base_url, params = params)
-# print r.url
-
-def extract_dom(start, url = 'http://www.imdb.com/search/title'):
-	params = dict(sort = 'num_votes', at = 0, start = start, title_type = 'feature', year = '1950,2014')
+# Helper functions
+#######################################################################
+def extract_dom(sort, start, url = 'http://www.imdb.com/search/title'):
+	params = dict(sort = sort, at = 0, start = start, title_type = 'feature', year = '1950,2014')
 	r = requests.get(url, params = params)
 	print r.url
 	return web.Element(r.text)
+
+def extract_runtime(div):
+	runtime = div.content
+	if runtime:
+		runtime = runtime.replace(' mins.', '')
+		return int(runtime)
+	else return 0
 
 def extract_rating(div):
 	rating_str = div.title
@@ -36,21 +51,65 @@ def extract_num_votes(div):
 	num_votes = num_votes_str[start_idx + 1 : end_idx]
 	return int(num_votes.strip('votes ').replace(',', ''))
 
-# For some reason, IMDB doesn't return results > 100,000, so I am arbitrarily setting it to 50,000 for now
-for at in range(1, 50000, NUMBER_MOVIES_PER_PAGE):
-	print "Currenting processing page number: " + str(at) + "..."
-	dom = extract_dom(at, base_url)
+def extract_box_office_number(elem):
+	box_office_number_str = elem.content
+	box_office_number = float(re.sub('[$M]', '', box_office_number_str)) * ONE_MILLION
+	return box_office_number
 
-	for movie in dom.by_tag('td.title'):
-		title = movie.by_tag('a')[0].content
-		year = movie.by_tag('span.year_type')[0].content.strip('(').strip(')')
-		genres = movie.by_tag('span.genre')[0].by_tag('a')
-		genres = [g.content for g in genres]
-		runtime = movie.by_tag('span.runtime')[0].content
-		rating_list = movie.by_tag('div.rating-list')[0]
-		# get rating
-		rating = extract_rating(rating_list)
-		# get votes
-		num_votes = extract_num_votes(rating_list)
-		# print the results
-		print title, year, genres, runtime, rating, votes
+def extract_crew_names(elem):
+	people = elem.by_tag('a')
+	crews = [person.content.encode('utf') for person in people]
+	return crews
+
+def extract_certificate(elem):
+	if elem.by_tag('span'):
+		certificate = elem.by_tag('span')[0].title
+		return certificate
+	else:
+		return 'NULL'
+
+# Scrape movie data - sorted by num_votes
+#######################################################################
+f = open('movie_data.csv', 'wb')
+writer = csv.writer(f)
+
+try:
+	# For some reason, IMDB doesn't return results > 100,000, so I am arbitrarily setting it to 50,000 for now
+	for start in range(1, 100 * 1000, NUMBER_MOVIES_PER_PAGE):
+		print "Currenting processing page number: " + str(start) + "..."
+		dom = extract_dom(sort = 'num_votes', start = start, url = base_url)
+		for movie in dom.by_tag('td.title'):
+			title = movie.by_tag('a')[0].content.encode('utf')
+			year = movie.by_tag('span.year_type')[0].content.strip('(').strip(')')
+			genres = movie.by_tag('span.genre')[0].by_tag('a')
+			genres = [g.content for g in genres]
+			runtime = extract_runtime(movie.by_tag('span.runtime')[0])
+			rating_list = movie.by_tag('div.rating-list')[0]
+			# get rating
+			rating = extract_rating(rating_list)
+			# get votes
+			num_votes = extract_num_votes(rating_list)
+			# print the results
+			print title, year, genres[0], runtime, rating, num_votes
+			writer.writerow((title, year, genres[0], runtime, rating, num_votes))
+finally:
+	f.close()
+
+# Scrape movie data - sorted by box office numbers
+#######################################################################
+f = open('movie_box_office.csv', 'wb')
+writer = csv.writer(f)
+
+try:
+	for start in range(1, 100 * 1000, NUMBER_MOVIES_PER_PAGE):
+		print bcolors.WARNING + "Currently processing box office page number: " + str(start) + "..." + bcolors.ENDC
+		dom = extract_dom(sort = 'boxoffice_gross_us', start = start, url = base_url)
+		for movie in dom.by_tag('tr.*detailed'):
+			box_office_number = extract_box_office_number(movie.by_tag('td.sort_col')[0])
+			title = movie.by_tag('a')[0].title[:movie.by_tag('a')[0].title.rfind('(') - 1].encode('utf')
+			crews = extract_crew_names(movie.by_tag('span.credit')[0])
+			certificate = extract_certificate(movie.by_tag('span.certificate')[0])
+			print title, box_office_number, crews[0], crews[1], crews[2], crews[3], certificate
+			writer.writerow((title, box_office_number, crews[0], crews[1], crews[2], crews[3], certificate))
+finally:
+	f.close()
